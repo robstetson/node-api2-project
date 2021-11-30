ea393ad953728cbd19b77d3bdf553bd1e614f461
robstetson <rstetsonx@gmail.com>
2021-11-30 17:36:06 -0500

    posts router started

--- api/server.js
+++ api/server.js
@@ -3,8 +3,11 @@
 
 
 const express = require('express')
+const postsRouter = require('./posts/posts-router')
 const server = express()
+
 server.use(express.json)
+server.use('/api/posts', postsRouter)
 
 server.use('*', (req, res)=>{
     res.status(404).json({
