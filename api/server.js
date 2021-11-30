b50b2c94f1c217319dac0e6bfe9b33c512a55a31
robstetson <rstetsonx@gmail.com>
2021-11-30 17:31:41 -0500

    some parts working, first commit

--- api/server.js
+++ api/server.js
@@ -1,2 +1,15 @@
 // implement your server here
 // require your posts router and connect it here
+
+
+const express = require('express')
+const server = express()
+server.use(express.json)
+
+server.use('*', (req, res)=>{
+    res.status(404).json({
+        message:'Not Found'
+    })
+})
+
+module.exports = server
