74a59882f9b10a150659eb170514191ae93aafbf
robstetson <rstetsonx@gmail.com>
2021-11-30 19:13:35 -0500

    All tests passing and end points working as intended

--- api/server.js
+++ api/server.js
@@ -6,13 +6,7 @@ const express = require('express')
 const postsRouter = require('./posts/posts-router')
 const server = express()
 
-server.use(express.json)
+server.use(express.json())
 server.use('/api/posts', postsRouter)
-
-server.use('*', (req, res)=>{
-    res.status(404).json({
-        message:'Not Found'
-    })
-})
 
 module.exports = server
